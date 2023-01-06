3e3f6b328c6fc0c0a18994843d3328ecf0e8b98d
Dionatan <dionatan.1751@gmail.com>
2023-01-06 09:02:37 -0300

    csv to html to pdf

---
+++ app.js
@@ -0,0 +1,25 @@
+const Reader = require("./Reader")
+const Writer = require("./Writer")
+const Processor = require("./Processor")
+const Table = require("./Table")
+const HtmlParser = require("./HtmlParser")
+const PDFWriter = require("./PDFWriter")
+
+var leitor = new Reader()
+var escritor = new Writer()
+
+
+async function main() {
+    var dados = await leitor.Read("./users.csv")
+    var dadosProcessados = Processor.Process(dados)
+
+    var usuarios = new Table(dadosProcessados)
+    
+    var html = await HtmlParser.Parse(usuarios)
+
+   escritor.Writer(Date.now() + ".html",html)
+   PDFWriter.WritePDF(Date.now() + ".PDF", html)
+
+}
+
+main()
